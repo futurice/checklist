b24f02b9df708271b6576d7d15291fc5914c3c1e
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-11-21 12:10:15 +0200

    Out with the old

--- employee/context_processors.py
+++
@@ -1,6 +0,0 @@
-from employee.views import determine_group
-
-def get_userinfo(request):
-    username = request.META["REMOTE_USER"]
-    group = determine_group(username)
-    return {'username': username, 'group': group}
