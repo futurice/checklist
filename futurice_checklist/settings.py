0e965bc44bcff9904fe48932f547375e0d27e6ba
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-11-24 14:29:50 +0200

    URL updates

--- futurice_checklist/settings.py
+++ futurice_checklist/settings.py
@@ -54,7 +54,7 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_ROOT, 'data.sqlite'),
     }
 }
 
